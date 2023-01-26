25578fdcb77cc340fc8511b05ab0c5265686a691
league <league@imac-24.attlocal.net>
2023-01-25 16:44:20 -0800

    finished module

--- src/_04_spot_the_error/MethodCall.java
+++ src/_04_spot_the_error/MethodCall.java
@@ -4,7 +4,7 @@ public class MethodCall {
 
 	// This program is supposed to print the message "Hello World".
 	// BUT it's not working. If you don't believe me, try running it.
-	// Add ONE line of code to fix this program.
+	
 	
 	public static void main(String[] args) {
 		
